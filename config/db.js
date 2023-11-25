ee6e9be448a6ffc5b02d9bd7e92aff504d3fbdcf
Mine61278924 <josmaria22@hotmail.com>
2023-11-24 20:28:34 -0400

    commit tercera

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config({path: 'variables.env'});
 
 const conectarDB = async ()=>{
     try {
+        mongoose.set('strictQuery', true);
         await mongoose.connect(process.env.DB_MONGO,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
